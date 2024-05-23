7e7e463c2d462b74c13af9872cef879109f6522c
Dante <68436988+DantePastorino@users.noreply.github.com>
2024-05-23 18:44:08 -0300

    Update ejercicio2.m

--- ejercicio2.m
+++ ejercicio2.m
@@ -3,36 +3,43 @@ clear
 close all
 
 % dt: Intervalo de tiempo entre muestras.
+%Se define el intervalo de tiempo (dt) entre cada muestra de la señal.
 dt = 0.0003;
 
 % Nx: Número total de muestras de la señal x(t) para formar la señal x[n]
-Nx = 201;
+%Se define el número total de muestras (Nx) para la señal.Nx = 201;
 
-% t: Define los puntos en el tiempo continuo para evaluar la señal analógica x(t).
+% t: Se crea un vector t que contiene los puntos en el tiempo continuo, desde 0 hasta (Nx-1)*dt, con un total de Nx puntos..
 t = 0:dt:(Nx-1)*dt; % Ajuste para asegurar que t tiene exactamente Nx puntos
 
 % Señal analógica original en tiempo continuo.
+%Se genera la señal analógica x_analog como una combinación de una cosenoidal de 50 Hz y una senoidal de 500 Hz.
 x_analog = 2*cos(2*pi*50*t) + 3*sin(2*pi*500*t);
 
 % Vector de tiempo discreto para la señal de entrada. Define los puntos en el tiempo discreto para la señal de entrada x[n]
+%Se crea un vector n que contiene los índices de tiempo discreto, desde 0 hasta Nx-1.
 n = 0:Nx-1;
 
 % Señal de entrada en tiempo discreto. x[n]
+%Se genera la señal x en tiempo discreto usando los mismos componentes que la señal analógica, pero evaluada en los puntos de tiempo discreto n*dt.
 x = 2*cos(2*pi*50*n*dt) + 3*sin(2*pi*500*n*dt);
 
 % Frecuencia de corte para el filtro. (Entre 50Hz y 500Hz)
+%Se define la frecuencia de corte Fc del filtro en 200 Hz.
 Fc = 200;
 
 % Longitud de la respuesta al impulso h[n]
 Nh = 101; % Debería ser impar y suficientemente grande
 
 % Vector de tiempo discreto para la respuesta al impulso h[n].
+%Se crea un vector nh que contiene los índices de tiempo discreto para la respuesta al impulso, centrado en cero.
 nh = -(Nh-1)/2:(Nh-1)/2;
 
 % Respuesta al impulso del sistema.
 h = 2 * Fc * dt * sinc(2 * Fc * dt * nh);
 
 % Salida del sistema LTI
+%utilizando la función sinc ajustada a la frecuencia de corte Fc.
 y = conv(x, h);
 
 % Vector de tiempo discreto para la salida y[n]
